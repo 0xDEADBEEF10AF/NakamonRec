2e1f07811737ee5f7549c864c168294fe6ae8cec
0xBEEF10AF <gutsherrschaft@gmail.com>
2026-02-23 13:03:02 +0900

    Initial commit for Public Release

---
+++ opencv/build/intermediates/cxx/Debug/372l2z4m/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\gutsh\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\gutsh\\AndroidStudioProjects\\NakamonRec\\opencv\\.cxx\\Debug\\372l2z4m\\arm64-v8a" ^
+  opencv_jni_shared
